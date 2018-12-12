602c5d2774aa4066e460b117f4e4e3bcaccf3752
dowster <dow.brady95@gmail.com>
2018-12-11 20:33:26 -0600

    Reading prat of the network thingy.

---
+++ Program5/src/net/dowster/school/datacomm/program5/LSRouting.java
@@ -0,0 +1,34 @@
+package net.dowster.school.datacomm.program5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LSRouting
+{
+   public static void main(String[] arrrrgs)
+   {
+      int network[][];
+      int startRouter;
+
+      Scanner inputScanner = new Scanner(System.in);
+
+      int networkSize = inputScanner.nextInt();
+      network = new int[networkSize][networkSize];
+
+      while(inputScanner.hasNextLine()) {
+         int[] currentLine =
+               Arrays.stream(inputScanner.nextLine().split(" "))
+                     .mapToInt(Integer::parseInt).toArray();
+
+         startRouter = currentLine[0];
+
+         for(int link = 1; link < currentLine.length - 1; link++ )
+            network[currentLine[0]][currentLine[link]] = currentLine[++link];
+      }
+
+
+
+
+
+   }
+}
