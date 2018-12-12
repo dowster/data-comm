c1805b3f64ee9e4335d2f43d017b3c7c93779c45
dowster <dow.brady95@gmail.com>
2018-12-11 21:06:45 -0600

    Reading prat of the network thingy.

--- Program5/src/net/dowster/school/datacomm/program5/LSRouting.java
+++ Program5/src/net/dowster/school/datacomm/program5/LSRouting.java
@@ -1,5 +1,7 @@
 package net.dowster.school.datacomm.program5;
 
+import javafx.util.Pair;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -7,13 +9,13 @@ public class LSRouting
 {
    public static void main(String[] arrrrgs)
    {
-      int network[][];
+      Pair<Integer, Integer> network[][];
       int startRouter = -1;
 
       Scanner inputScanner = new Scanner(System.in);
 
       int networkSize = inputScanner.nextInt();
-      network = new int[networkSize][networkSize];
+      network = new Pair[networkSize][networkSize];
 
       // Clear the rest of the first line.
       inputScanner.nextLine();
@@ -26,9 +28,24 @@ public class LSRouting
          startRouter = currentLine[0];
 
          for(int link = 1; link < currentLine.length - 1; link++ )
-            network[currentLine[0] - 1][currentLine[link] - 1] = currentLine[++link];
+            network[currentLine[0] - 1][currentLine[link] - 1] = new Pair(currentLine[link], currentLine[++link]);
       }
 
+      sortNetwork(network, startRouter);
+
+
       System.out.println("Yo we done, start at: " + startRouter);
+   }
+
+   private static void sortNetwork(Pair<Integer, Integer>[][] network, int startRouter)
+   {
+      // loop
+         // find w not in N` such that D(w) is a minimum
+         // add w to N`
+         // update D(v) for each neighbor V of w and not in N`:
+            //D(v) = min( D(v), D(w) + c(w,v) )
+         /* new cost to v is either old cost to v or known least path
+            least path cost to w plus cost from w to v */
+      // until N` = N
    }
 }
