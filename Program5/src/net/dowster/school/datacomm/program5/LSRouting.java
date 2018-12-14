38b4bdbccc4eb83dea5b54a8fcc00a7c6dd5c7b3
dowster <dow.brady95@gmail.com>
2018-12-14 16:03:21 -0600

    Final comments.

--- Program5/src/net/dowster/school/datacomm/program5/LSRouting.java
+++ Program5/src/net/dowster/school/datacomm/program5/LSRouting.java
@@ -2,11 +2,45 @@ package net.dowster.school.datacomm.program5;
 
 import javafx.util.Pair;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Stream;
 
+/**
+ * Console program which calculates the least cost routes for a specific router
+ * on a network using link state routing algorithm.
+ *
+ * The input should be formatted with three types of lines:
+ *    Type 1 - Network Size, occurs once:
+ *       This line contains a single integer which is the number of routers in
+ *       the network.
+ *
+ *    Type 2 - Neighbor Costs, can occur any number of times after type 1:
+ *       This line starts with the integer ID of a router in the network. Then
+ *       for each neighbor of the router the line contains the ID and cost pair
+ *       for the link.
+ *       Example: 5 7 19 8 20 3 1
+ *                Router 5 has three neighbors:
+ *                   Router 7 is a neighbor with link cost of 19
+ *                   Router 8 is a neighbor with link cost of 20
+ *                   Router 3 is a neighbor with link cost of 01
+ *
+ *    Type 3 - Start Router, must be the last line of the input:
+ *       This line contains a single integer which is the router to sort and
+ *       generate least cost paths for.
+ */
 public class LSRouting
 {
+   /**
+    * Reads in network info, sorts the network around the start router, and then
+    * prints the least cost paths to all other routers in the network.
+    *
+    * @param arrrrgs unused
+    */
    public static void main(String[] arrrrgs)
    {
       Pair<Integer, Integer> network[][];
@@ -23,26 +57,26 @@ public class LSRouting
       inputScanner.nextLine();
 
       while(inputScanner.hasNextLine()) {
-         int[] currentLine =
+         int[] line =
                Arrays.stream(inputScanner.nextLine().split(" "))
                      .mapToInt(Integer::parseInt).toArray();
 
-         startRouter = currentLine[0];
+         startRouter = line[0];
 
-         for(int link = 1; link < currentLine.length - 1; link++ )
-            network[currentLine[0] - 1][currentLine[link] - 1] = new Pair(currentLine[link], currentLine[++link]);
+         for(int link = 1; link < line.length - 1; link++ )
+            network[line[0] - 1][line[link] - 1] =
+                  new Pair(line[link], line[++link]);
       }
 
       sortNetwork(network, startRouter);
-
       printPath(network, startRouter);
    }
 
    /**
     * Prints out all the shortest paths from startRouter to all other routers.
     *
-    * @param net
-    * @param start
+    * @param net The network matrix (with least cost paths calculated for start)
+    * @param start The router to start at
     */
    private static void printPath(Pair<Integer, Integer>[][] net, int start)
    {
@@ -62,8 +96,8 @@ public class LSRouting
     * The result of this is that net[strt - 1] is an array of the shortest
     * paths. All other net[n] arrays are unsorted.
     *
-    * @param net
-    * @param strt
+    * @param net  the matrix of routers and path costs to sort
+    * @param strt the router to calculate least cost paths for (could be any)
     */
    private static void sortNetwork(Pair<Integer, Integer>[][] net, int strt)
    {
@@ -120,12 +154,13 @@ public class LSRouting
     * End condition should be when:
     *     net[start - 1][nLast - 1] == nLast;
     *
-    * @param network
-    * @param start
-    * @param dest
-    * @return
+    * @param network the matrix of routers and route costs
+    * @param start   the router to start the path at
+    * @param dest    the router to end the path at
+    * @return a stream of the router IDs along the path from start to end
     */
-   private static Stream<Integer> getPath(Pair<Integer,Integer>[][] network, int start, int dest) {
+   private static Stream<Integer> getPath(
+         Pair<Integer,Integer>[][] network, int start, int dest) {
       // If the start == the dest we don't need to search the network, router
       // should have a zero cost path to itself. If not, get off my network.
       if(start == dest)
